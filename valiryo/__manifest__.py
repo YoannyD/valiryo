bd417b915ae4ebe28914a42650c69601d2a1168d
adr9 <a.delrio@ingetive.com>
2021-02-11 07:14:20 +0000

    relacionar productos con etiquetas crm

---
+++ valiryo/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# © 2021 Ingetive - <info@ingetive.com>
+{
+    'name': "Valiryo",
+    'summary': """""",
+    'description': """""",
+    'author': "Ingetive",
+    'website': "http://ingetive.com",
+    'category': 'Uncategorized',
+    'version': '13.0.1.0.1',
+    'depends': [
+        'base',
+    ],
+    'data': [
+        'views/product_template.xml'
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
+
