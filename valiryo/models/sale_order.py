d81d8a3df22407ea8f4a130e8f7d376aaa95e9c0
Adrian <adrian@voodoo.es>
2022-01-14 10:17:29 +0000

    transferencia bancaria en PV

--- valiryo/models/sale_order.py
+++ valiryo/models/sale_order.py
@@ -16,3 +16,32 @@ class SaleOrder(models.Model):
     
     sale_country_id = fields.Many2one("res.country", string="País pedido")
     marketplace = fields.Boolean("Marketplace")
+    transferencia_bancaria = fields.Boolean("Transferencia bancaria")
+    bank_id = fields.Many2one('res.partner.bank', string='Cuenta bancaria', check_company=True)
+    company_partner_id = fields.Many2one("res.partner", compute="_compute_company_partner", 
+                                         string="Contacto compañia")
+    
+    @api.onchange('partner_id')
+    def _onchange_partner_id_warning(self):
+        res = super(SaleOrder, self)._onchange_partner_id_warning()
+        if not self.partner_id:
+            return res
+        
+        self = self.with_company(self.company_id)
+        if self.partner_id.bank_id:
+            self.bank_id = self.partner_id.bank_id.id
+            
+        self.transferencia_bancaria = self.partner_id.transferencia_bancaria
+        return res
+    
+    def _compute_company_partner(self):
+        for r in self:
+            r.company_partner_id = self.env.company.partner_id.id
+            
+    def _prepare_invoice(self):
+        invoice_vals = super(SaleOrder, self)._prepare_invoice()
+        invoice_vals.update({
+            'transferencia_bancaria': self.transferencia_bancaria,
+            'partner_bank_id': self.bank_id.id
+        })
+        return invoice_vals
---
+++ valiryo/wizard/sale_make_invoice_advance.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# © 2021 Ingetive - <info@ingetive.com>
+
+import logging
+
+from odoo import api, fields, models, _
+
+_logger = logging.getLogger(__name__)
+
+
+class SaleAdvancePaymentInv(models.TransientModel):
+    _inherit = "sale.advance.payment.inv"
+    
+    def _create_invoice(self, order, so_line, amount):
+        _logger.warning("_create_invoice")
+        return super(SaleAdvancePaymentInv, self)._create_invoice(order, so_line, amount)
+    
+    def _prepare_invoice_values(self, order, name, amount, so_line):
+        res = super(SaleAdvancePaymentInv, self)._prepare_invoice_values(order, name, amount, so_line)
+        res.update({
+            'transferencia_bancaria': order.transferencia_bancaria,
+            'partner_bank_id': order.bank_id.id
+        })
+        return res
+        
+    
