2301c6fbf32789312dca6fe93d7874994c0aa699
sistemastetrace <sistemas@tetrac.com>
2022-03-07 12:34:14 +0000

    cambios crm_lead

--- valiryo/models/__init__.py
+++ valiryo/models/__init__.py
@@ -6,3 +6,4 @@ from . import sale_order
 from . import stock_picking
 from . import res_partner
 from . import account_move
+from . import crm_lead
--- valiryo/models/crm_lead.py
+++ valiryo/models/crm_lead.py
@@ -8,8 +8,7 @@ from odoo import models, fields, api
 _logger = logging.getLogger(__name__)
 
 
-class crm_lead(models.Model):
-    _inherit = "crm.lead"
+class CrmLead(models.Model):
+    _inherit = 'crm.lead'
     
-    categoria_cliente = fields.Many2many('res.partner.category',related='res_partner_category_id', store=True)
-    realizado = fields.Boolean("Realizado")
+    categoria_cliente = fields.Many2many(string='Categoria Cliente', related='partner_id.category_id')
